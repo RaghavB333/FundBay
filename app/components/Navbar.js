548b03ae679616a3d84bc91938e23cb1e2150a26
RaghavB333 <89807017+RaghavB333@users.noreply.github.com>
2025-04-06 14:20:08 +0530

    Update Navbar.js

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -71,7 +71,7 @@ const Navbar = () => {
                 </li>
                 <li>
 <Link
-  href={`${process.env.NEXT_PUBLIC_URL}/${session.user.name}`}
+  href={`/${session.user.name}`}
   className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
 >
   Your Page
